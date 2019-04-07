affab975160cf954098e5edb4b401e42b0b9d492
lonyele <skyrabbits1@gmail.com>
2019-04-07 14:37:52 +0900

    Add docs(local code theme) to example(Custom Code Theme)

--- packages/example-react/stories/withCustomCodeTheme.js
+++ packages/example-react/stories/withCustomCodeTheme.js
@@ -11,14 +11,14 @@ const GlobalCodeThemeMD = `
 You can set a code theme globally by adding \`codeTheme\` value to \`addParameters\`
 at \`config.js\`.
 
-\`\`\`
+\`\`\`javascript
 import { addParameters, addDecorator, configure } from '@storybook/react';
 import { addReadme } from 'storybook-readme';
 
 addParameters({
   readme: {
     // You can set the global code theme here. 
-    codeTheme: 'dracula'
+    codeTheme: 'github'
   }
 });
 
@@ -40,6 +40,33 @@ ex) 'Dracula' -> 'dracula'
     'Xt 256' -> 'xt256'
 \`\`\`
 `
+
+const LocalCodeThemeMD = `
+# Local Code Theme
+
+You can override global code theme by adding \`codeTheme\` value to \`addParameters\`
+at story.
+
+\`\`\`javascript
+storiesOf('Your story name here', module)
+  .addParameters({
+    readme: {
+      codeTheme: 'dracula',
+    },
+  })
+  .add('Your component name', () => <div>Your component</div>)
+
+\`\`\`
+
+Full lists of themes are at https://highlightjs.org/static/demo/.
+To be used with storybook-readme, naming of the code theme should be used in one of these styles. [codeTheme names](https://github.com/tuchk4/storybook-readme/tree/master/packages/storybook-readme/src/styles/codeThemes)
+\`\`\`
+ex) 'Dracula' -> 'dracula'
+    'Atelier Sulphurpool Dark' -> 'atelier-sulphurpool-dark'
+    'Xt 256' -> 'xt256'
+\`\`\`
+`
+
 storiesOf('Custom Code Theme', module)
   .addParameters({
     readme: {
@@ -48,3 +75,11 @@ storiesOf('Custom Code Theme', module)
     },
   })
   .add('Global(all stories)', () => null)
+  .addParameters({
+    readme: {
+      codeTheme: 'dracula',
+      content: LocalCodeThemeMD,
+      sidebar: ButtonReadme,
+    },
+  })
+  .add('Local(specific story)', () => null)
